f6fbcc173a5c7e46a9da9fdb07187ba3edb60c5e
HAL-9001\AGNIV <acalphabeta13@gmail.com>
2023-10-02 23:15:42 +0530

    shell script edits

--- fetch_data.sh
+++ fetch_data.sh
@@ -15,3 +15,4 @@ wget https://keeper.mpdl.mpg.de/f/9cb970221b1e45d185b8/?dl=1 --max-redirect=2 --
 # Downloading training datasets
 mkdir datasets
 wget https://keeper.mpdl.mpg.de/f/81c3ec9997dd440b8db3/?dl=1 --max-redirect=2 --trust-server-names  && tar -xvf Release_Datasets.tar.gz --directory datasets && rm -r Release_Datasets.tar.gz
+
